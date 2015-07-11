82cc63c6fb6f01bc554e10e80a5890cc5f704622
crisvill <alejandro201@gmail.com>
2015-07-10 22:48:12 -0430

    Validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -26,14 +26,14 @@ exports.index = function(req, res) {
  // Busca en la colección los datos que concuerde con search
   models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
     function(quizes) {
-        res.render('quizes/index.ejs', { quizes: quizes});
+        res.render('quizes/index.ejs', { quizes: quizes, errors: []});
     }
   ).catch(function(error) { next(error);});
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-    res.render('quizes/show', { quiz: req.quiz});
+    res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -42,12 +42,12 @@ exports.answer = function(req, res) {
     if (req.query.respuesta === req.quiz.respuesta) {
       resultado = 'Correcto';
     }
-    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 /* GET /author*/
 exports.author = function(req, res) {
-  res.render('author', {autor: 'Elon Musk'});
+  res.render('author', {autor: 'Elon Musk', errors: []});
 };
 
 /* GET /new */
@@ -56,15 +56,26 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  });   // res.redirect: Redirección HTTP a lista de preguntas
+  quiz
+  .validate()
+  .then(
+    function(err) {
+      if(err){
+        res.render("quizes/new", {quiz: quiz , errors: err.errors});
+      } else {
+        quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then(function(){
+          res.redirect('/quizes');
+        });   // res.redirect: Redirección HTTP a lista de preguntas
+      }
+    }
+  );
 };
