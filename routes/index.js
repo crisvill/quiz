b174709d4f1b11cf79948c0a1008d24e822772cd
crisvill <alejandro201@gmail.com>
2015-08-10 00:08:52 -0430

    Autenticación de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 /* Importa el controlador "quiz_controller"*/
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+
+//Definicion de rutas de Session
+router.get('/login',    sessionController.new); //carga formulario "login"
+router.post('/login',   sessionController.create); //crea sesion
+router.get('/logout',   sessionController.destroy); //destruye sesion
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -24,6 +30,7 @@ router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 router.get('/:categoria',                  quizController.indexCat);
 
+//Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
